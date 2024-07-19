cdbd7409c9b30c35e0fcd61bb70f6207a825daaf
Shailesh Prajapati <shaileshprajapati272001@gmail.com>
2024-07-19 13:55:12 +0530

    quick fix for day 7

--- Day_7_Objects/activity2.js
+++ Day_7_Objects/activity2.js
@@ -1,30 +1,24 @@
-
 // ************************ Object Methods **************************
-
 
 // Task 3: Add a method to the book object that returns a string with the book's title and author, log the result of calling this method.
 
-
 let book = {
-    title : "Rich Dad Poor Dad",
-    author : "Robert Kiyosaki and Sharon Lechter",
-    year : 1997,
-    getInfo: function(){
-        return this.title + " by " + this.author
-    },
-    updateYear: function(newYear){
-        this.year = newYear
-    }
-}
+  title: "Rich Dad Poor Dad",
+  author: "Robert Kiyosaki and Sharon Lechter",
+  year: 1997,
+  getInfo: function () {
+    return this.title + " by " + this.author;
+  },
+  updateYear: function (newYear) {
+    this.year = newYear;
+  },
+};
 
 console.log(book.getInfo());
 
-
-
 // Task 4: Add a method to the book object that takes a parameter(year) and updates the book's year property, then log the updated object.
-
 
 // Call the method to update the year
 book.updateYear(2012);
 
-console.log(bookk);
+console.log(book);
